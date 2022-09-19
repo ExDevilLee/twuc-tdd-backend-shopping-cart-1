56d52baf8ad97ae5ed3b377227422155d82fcb47
Songlin Li <slli@thoughtworks.com>
2022-09-19 16:31:09 +0800

    test: add test for ShoppingCartControllerTest

--- src/test/java/com/thoughtworks/capability/web/ShoppingCartControllerTest.java
+++ src/test/java/com/thoughtworks/capability/web/ShoppingCartControllerTest.java
@@ -5,10 +5,8 @@ import com.thoughtworks.capability.domain.Product;
 import com.thoughtworks.capability.service.ShoppingCartService;
 import com.thoughtworks.capability.web.dto.ShoppingCartResponse;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,6 +15,7 @@ import java.math.BigDecimal;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -47,5 +46,26 @@ public class ShoppingCartControllerTest extends WebApplicationTest {
                 .andExpect(jsonPath("$.products").isEmpty())
                 .andExpect(jsonPath("$.totalPrice").exists())
                 .andExpect(jsonPath("$.totalPrice").value(BigDecimal.ZERO));
+    }
+
+    @Test
+    public void shouldReturnTwoProductsInShoppingCartWhenHasData() throws Exception {
+        // given
+        Product p1 = Product.builder().id(1L).name("p1").price(BigDecimal.valueOf(10)).build();
+        Product p2 = Product.builder().id(2L).name("p2").price(BigDecimal.valueOf(20)).build();
+        ShoppingCartResponse response = new ShoppingCartResponse(asList(p1, p2), BigDecimal.valueOf(30));
+        when(service.showShoppingCart()).thenReturn(response);
+
+        // when
+        mvc.perform(MockMvcRequestBuilders
+                .get("/shoppingCart")
+                .accept(MediaType.APPLICATION_JSON))
+                // then
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.products").exists())
+                .andExpect(jsonPath("$.products").isArray())
+                .andExpect(jsonPath("$.products[0].name").value("p1"))
+                .andExpect(jsonPath("$.totalPrice").exists())
+                .andExpect(jsonPath("$.totalPrice").value(BigDecimal.valueOf(30)));
     }
 }
